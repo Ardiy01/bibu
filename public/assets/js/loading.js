2de51b346d557fdc23e587cfce85a9b46ea91de8
Ardiy01 <202410101051@mail.unej.ac.id>
2022-05-17 11:39:15 +0700

    halaman loading

--- public/assets/js/loading.js
+++ public/assets/js/loading.js
@@ -5,12 +5,6 @@ $(document).ready(function() {
         $('.load').append(
             '<div class="text-center my-5 d-flex justify-content-center align-self-center loading "><div class="row"><div class="mt-5 mb-0 col-12 align-self-center"><img src="https://i.ibb.co/0jCwqKd/Logo.png" alt="logobibu" class="loader"><h5 class="my-5 fw-bold" style="color: #007C84;">Harap Tunggu....</h5></div></div></div>'
         );
-        $('body').css({'filter':'blur(0.6px)'});
         $('.sidebar').css({'position':'fixed'});
-    });
-    $('#search').click(function() {
-        $('.load').append(
-            '<div class="text-center my-5 d-flex justify-content-center align-self-center loading "><div class="row"><div class="mt-5 mb-0 col-12 align-self-center"><img src="https://i.ibb.co/0jCwqKd/Logo.png" alt="logobibu" class="loader"><h5 class="my-5 fw-bold" style="color: #007C84;">Harap Tunggu....</h5></div></div></div>'
-        );
     });
 });
